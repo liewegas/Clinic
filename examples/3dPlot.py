e8b3e43634031e0c387b53e28b62a26faa661676
Nick Carter <njc093@gmail.com>
2015-04-28 03:30:29 -0700

    working on documentation

--- examples/3dPlot.py
+++ examples/3dPlot.py
@@ -15,18 +15,17 @@ import csv
 from mpl_toolkits.mplot3d import Axes3D
 from numpy import log10
 
+# Boolean to turn on and off captioning of figures
+CAPTION = False
+
 # for floating point comparisons
 threshold = 1e-11
 
-# TODO:
-#   Get plot_surface to work, it should work and would be better than the triangle approach
-#   Verify that setting static points is working properly
-#   Make latency into ms, not s
-#   Get z setting labeling to work
-
 
 class ColSettings:
-    """ Settings object to contain properties of a data dimension """
+    """ Settings object to contain properties of a data dimension.
+        Simply wraps several parameters that each data dimension must
+        specify. """
 
     def __init__(self, name, scale="linear", 
                  defaultValue=0):
@@ -35,9 +34,13 @@ class ColSettings:
         self.defaultValue = defaultValue
 
 
-# Rows at the beginning of the file while the protocol is still trying to figure out what it should be doing
+# Rows at the beginning of the file to drop.
+# These should be the lines we want to throw out 
+# while the protocol is still trying to figure out what it should be doing
 firstRowsOfJunk = 1500
-# Settings of computed data dimensions. A set of graphs will be computed for each computed data dimension.
+
+# Settings of computed data dimensions. A set of graphs will be computed 
+# for each computed data dimension.
 computedColSettings = [ColSettings("Mean of NTP Uncertainty (ms)"),
                        ColSettings("Max of NTP Uncertainty (ms)"),
                        ColSettings("STD of NTP Uncertainty (ms)"),
@@ -58,9 +61,8 @@ dimensionsOfComputedData = len(computedColSettings)
 
 
 def loadFile(fd, cols=(1,)):
-    """ returns a single double that results from applying 
-        reductionFunc to the array found at path.
-        Selects cols from the file at path, by default col 1 (the second col) """
+    """ Returns cols columns of the file at file descriptor fd. 
+        By default, just pulls the second column of the file. """
 
     fileData = numpy.loadtxt(
         fd,
@@ -72,77 +74,117 @@ def loadFile(fd, cols=(1,)):
 
 
 def processFolder(rootpath, folder, dataArray, index):
-    """ path: str, dataArray: numpy array of the necessary order to hold data 
-        Currently requires 12 more dimensions than the number of parameters"""
+    """ Process some hard-coded files in a given folder according to 
+        some hard-coded transformations. Specify what values you are interested
+        in in this function. 
+
+        rootpath: str -- path to folder (not including folder), 
+        folder: str -- name of folder, 
+        dataArray: numpy array of the necessary order to hold data,
+        index: which dimension to save this set of values into """
 
     path = os.path.join(rootpath, folder)
     
-    # split the parameters and place them at the relevant index in the matrix
+    # Folders are named with the input parameters of the run they contain.
+    # Split the parameters and place them at the relevant index in the matrix.
     params = folder.split('_')
     paramsCount = len(params)
     for i in xrange(paramsCount):
         dataArray[index][i] = float(params[i])
 
+    # Open the files that are relevant to our calculations
     with open(os.path.join(path, "log.ntp_maxerror")) as fdError:
         with open(os.path.join(path, "log.ntp_offset")) as fdNTPOffset:
             with open(os.path.join(path, "log.timeoffset")) as fdRealOffset:
 
-                it = itertools.count(0)
+                # Each calculated value needs to go in its own column. 
+                # Let's use an iterator to count up. We start at the column
+                # after the last input parameter column.
+                it = itertools.count(paramsCount)
 
+                # For the parameters read in in this iteration of the simulation,
+                # calculate characteristic values. Save them to the appropriate
+                # place in the data array we are building.
+
+                # Error Data was reported in microseconds. Switch to milliseconds.
                 errorData = loadFile(fdError) / 1000.0
-                dataArray[index][paramsCount+it.next()] = numpy.mean(errorData)
-                dataArray[index][paramsCount+it.next()] = numpy.amax(errorData)
-                dataArray[index][paramsCount+it.next()] = numpy.std(errorData)
+                dataArray[index][it.next()] = numpy.mean(errorData)
+                dataArray[index][it.next()] = numpy.amax(errorData)
+                dataArray[index][it.next()] = numpy.std(errorData)
             
                 # take absolute values as offsets can be positive or negative
                 ntpOffsetData = numpy.fabs(loadFile(fdNTPOffset)) * 1000
-                dataArray[index][paramsCount+it.next()] = numpy.mean(ntpOffsetData)
-                dataArray[index][paramsCount+it.next()] = numpy.amax(ntpOffsetData)
-                dataArray[index][paramsCount+it.next()] = numpy.std(ntpOffsetData)
+                dataArray[index][it.next()] = numpy.mean(ntpOffsetData)
+                dataArray[index][it.next()] = numpy.amax(ntpOffsetData)
+                dataArray[index][it.next()] = numpy.std(ntpOffsetData)
 
                 # take absolute values as timeoffsets can be positive or negative
                 realOffsetData = numpy.fabs(loadFile(fdRealOffset)) * 1000
-                dataArray[index][paramsCount+it.next()] = numpy.mean(realOffsetData)
-                dataArray[index][paramsCount+it.next()] = numpy.amax(realOffsetData)
-                dataArray[index][paramsCount+it.next()] = numpy.std(realOffsetData)
+                dataArray[index][it.next()] = numpy.mean(realOffsetData)
+                dataArray[index][it.next()] = numpy.amax(realOffsetData)
+                dataArray[index][it.next()] = numpy.std(realOffsetData)
 
+                # The safety buffer is calculated from offset data and error data
                 safetyBufferData = errorData - numpy.fabs(ntpOffsetData - realOffsetData)
-                dataArray[index][paramsCount+it.next()] = numpy.mean(safetyBufferData)
-                dataArray[index][paramsCount+it.next()] = numpy.amax(safetyBufferData)
-                dataArray[index][paramsCount+it.next()] = numpy.amin(safetyBufferData)
-                dataArray[index][paramsCount+it.next()] = numpy.std(safetyBufferData)
+                dataArray[index][it.next()] = numpy.mean(safetyBufferData)
+                dataArray[index][it.next()] = numpy.amax(safetyBufferData)
+                dataArray[index][it.next()] = numpy.amin(safetyBufferData)
+                dataArray[index][it.next()] = numpy.std(safetyBufferData)
 
 
-
+    # Every once in awhile, report out an update on our progress.
     if (index % 100 == 0):
         print "Processed folder %s" % index
 
 
 
 def make3dPlotTrisurf(dataArray, cols, fixed, colSettings, captionText, pathRoot):
-    """ expects a 3-ple of cols """
+    """ Create and save a 3d graph using triangle interpolation to generate 
+        a surface. Uses cols parameter to select columns in dataArray to be 
+        graphed. 
 
+        dataArray:  The big array containing all of the data we have read in.
+        cols:       Which columns to use in constructing the current graph.
+                    Expected to be a 3-ple.
+        fixed:      An array of column indices that are to be held at fixed values for
+                    constructing this graph. The values to fix these columns at
+                    are looked up in colSettings.
+        colSettings:    An array that contains settings for each data dimension.
+        captionText:    Text to add to the graph, under the tile, probably useful
+                        for describing the parameters from which the graph was
+                        created.
+        pathRoot:   The folder in which to save the graph once it is constructed.
+        """
+
+    # Create a figure
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+
+    # Set the eye perspective
     ax.view_init(elev=13, azim=-30)
 
+    # Filter values we don't care about out of the data array
     filteredArray = dataArray
     for col in fixed:
         filteredArray = filteredArray[ (colSettings[col].defaultValue-threshold < filteredArray[:,col]) & 
                                        (colSettings[col].defaultValue+threshold > filteredArray[:,col]) ]
 
+    # Grab the columns we care about
     x = filteredArray[:,cols[0]]
     y = filteredArray[:,cols[1]]
     z = filteredArray[:,cols[2]]
 
+    # Grab the settings objects we care about
     xSettings = colSettings[cols[0]]
     ySettings = colSettings[cols[1]]
     zSettings = colSettings[cols[2]]
 
+    # Grab the names of the dimensions we care about
     xlabel = xSettings.name
     ylabel = ySettings.name
     zlabel = zSettings.name
 
+    # If we are working with an 'exotic' axis scale, set it up here.
     # Hack to work around https://github.com/matplotlib/matplotlib/issues/209
     if (xSettings.scale == 'log'):
         x = log10(x)
@@ -154,6 +196,7 @@ def make3dPlotTrisurf(dataArray, cols, fixed, colSettings, captionText, pathRoot
         z = log10(z)
         zlabel = 'Log10 of ' + zlabel
 
+    # Actually plot the surface
     surf = ax.plot_trisurf(x, y, z, cmap=cm.coolwarm, linewidth=0.1)
 
     # Customize plot
@@ -161,49 +204,81 @@ def make3dPlotTrisurf(dataArray, cols, fixed, colSettings, captionText, pathRoot
     plt.ylabel(ylabel)
     ax.set_zlabel(zlabel)
 
+    # Take the units and gunk off of the name strings -- some things don't need that stuff
     strippedNames = map(lambda x: x.split('(')[0].strip(), [zSettings.name, ySettings.name, xSettings.name])
 
+    # Make a note on the graph of the parameters that were fixed, and what the values were
     fixedText = "\nFixed Parameters = ("
     for col in fixed:
         fixedText += " " + colSettings[col].name + ": " + str(colSettings[col].defaultValue) + ","
     fixedText = fixedText[:-1] + " )"
 
+    # Build a reasonable title for this graph
+    # Graphs are about 60 title-characters wide, and don't word wrap automatically
     title = "{} vs {} vs {}".format(*strippedNames)
     if (len(title) > 60): 
         title = "{} vs {}\nvs {}".format(*strippedNames)
     plt.title(title)
 
-    #   commented out for plots for poster
-#    plt.figtext(.1, .8, captionText + fixedText, verticalalignment=u'baseline', size=u'xx-small')
+    # Actually insert the caption into the figure
+    if (CAPTION):
+        plt.figtext(.1, .8, captionText + fixedText, verticalalignment=u'baseline', size=u'xx-small')
+
+    # Name, save, and close the file
     filename = "{}_{}_{}_trisurf.png".format(*strippedNames)
     plt.savefig(os.path.join(pathRoot, filename))
     plt.close()
+
+    # Print out a status message
     print 'saved file to %s' % filename
 
 def make3dPlotScatter(dataArray, cols, fixed, colSettings, captionText, pathRoot):
-    """ expects a 3-ple of cols """
+    """ Create and save a 3d scatter plot of input values.
+        Uses cols parameter to select columns in dataArray to be 
+        graphed. 
 
+        dataArray:  The big array containing all of the data we have read in.
+        cols:       Which columns to use in constructing the current graph.
+                    Expected to be a 3-ple.
+        fixed:      An array of column indices that are to be held at fixed values for
+                    constructing this graph. The values to fix these columns at
+                    are looked up in colSettings.
+        colSettings:    An array that contains settings for each data dimension.
+        captionText:    Text to add to the graph, under the tile, probably useful
+                        for describing the parameters from which the graph was
+                        created.
+        pathRoot:   The folder in which to save the graph once it is constructed.
+        """
+
+    # Create a figure
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+
+    # Set the eye perspective
     ax.view_init(elev=13, azim=-30)
 
+    # Filter values we don't care about out of the data array
     filteredArray = dataArray
     for col in fixed:
         filteredArray = filteredArray[ (colSettings[col].defaultValue-threshold < filteredArray[:,col]) & 
                                        (colSettings[col].defaultValue+threshold > filteredArray[:,col]) ]
 
+    # Grab the columns we care about
     x = filteredArray[:,cols[0]]
     y = filteredArray[:,cols[1]]
     z = filteredArray[:,cols[2]]
 
+    # Grab the settings objects we care about
     xSettings = colSettings[cols[0]]
     ySettings = colSettings[cols[1]]
     zSettings = colSettings[cols[2]]
 
+    # Grab the names of the dimensions we care about
     xlabel = xSettings.name
     ylabel = ySettings.name
     zlabel = zSettings.name
 
+    # If we are working with an 'exotic' axis scale, set it up here.
     # Hack to work around https://github.com/matplotlib/matplotlib/issues/209
     if (xSettings.scale == 'log'):
         x = log10(x)
@@ -215,6 +290,7 @@ def make3dPlotScatter(dataArray, cols, fixed, colSettings, captionText, pathRoot
         z = log10(z)
         zlabel = 'Log10 of ' + zlabel
 
+    # Actually plot the surface
     surf = ax.scatter(x, y, z, cmap=cm.jet, linewidth=0.2)
 
     # import pdb; pdb.set_trace()
@@ -224,23 +300,32 @@ def make3dPlotScatter(dataArray, cols, fixed, colSettings, captionText, pathRoot
     plt.ylabel(ylabel)
     ax.set_zlabel(zlabel)
 
+    # Take the units and gunk off of the name strings -- some things don't need that stuff
     strippedNames = map(lambda x: x.split('(')[0].strip(), [zSettings.name, ySettings.name, xSettings.name])
 
+    # Make a note on the graph of the parameters that were fixed, and what the values were
     fixedText = "\nFixed Parameters = ("
     for col in fixed:
         fixedText += " " + colSettings[col].name + ": " + str(colSettings[col].defaultValue) + ","
     fixedText = fixedText[:-1] + " )"
 
+    # Build a reasonable title for this graph
     # Graphs are about 60 title-characters wide, and don't word wrap automatically
     title = "{} vs {} vs {}".format(*strippedNames)
     if (len(title) > 60): 
         title = "{} vs {}\nvs {}".format(*strippedNames)
     plt.title(title)
 
-    # plt.figtext(.1, .8, captionText + fixedText, verticalalignment=u'baseline', size=u'xx-small')
+    # Actually insert the caption into the figure
+    if (CAPTION):
+        plt.figtext(.1, .8, captionText + fixedText, verticalalignment=u'baseline', size=u'xx-small')
+    
+    # Name, save, and close the file
     filename = "{}_{}_{}_scatter.png".format(*strippedNames)
     plt.savefig(os.path.join(pathRoot, filename))
     plt.close()
+
+    # Print out a status message
     print 'saved file to %s' % filename
 
 
@@ -261,7 +346,6 @@ def main(rootpath, captionPath, outPath, defaults):
     for i in range(len(paramNamesList)):
         paramSettings.append(ColSettings(paramNamesList[i], scale=paramScaleList[i], defaultValue=defaultVals[i]))
 
-    # TODO: refactor
     # special setting for clock wander, axis units overlap axis label
     paramSettings[0].name = "\n\n" + paramSettings[0].name
 
@@ -280,11 +364,7 @@ def main(rootpath, captionPath, outPath, defaults):
     dataArray = numpy.empty((len(dirs), len(paramNamesList) + dimensionsOfComputedData))
 
     # fill out the array with the data in folders
-    # map(lambda index: processFolder(rootpath, dirs[index], dataArray, index), range(len(dirs)))
-    map(lambda index: processFolder(rootpath, dirs[index], dataArray, index), range(400))
-
-    # Do special processing on this data
-
+    map(lambda index: processFolder(rootpath, dirs[index], dataArray, index), range(len(dirs)))
 
     # make the graphs
     numberOfInputParams = len(paramNamesList)
--- examples/clinicsimplots.py
+++ examples/clinicsimplots.py
@@ -5,10 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 
-realOffsetFileName = "tmp/log.timeoffset"
-ntpOffsetFileName = "tmp/log.ntp_offset"
-maxErrorFileName = "tmp/log.ntp_maxerror"
-packetDelayFileName = "tmp/log.packetdelays"
+
+# These file paths would need to be changed if the simulation output is placed
+# somewhere else.
+realOffsetFilePath = "tmp/log.timeoffset"
+ntpOffsetFilePath = "tmp/log.ntp_offset"
+maxErrorFilePath = "tmp/log.ntp_maxerror"
+packetDelayFilePath = "tmp/log.packetdelays"
 
 def getValuesFromFile(fileName, valueIndex):
     with open(fileName, 'r') as theFile:
@@ -21,78 +24,76 @@ def getValuesFromFile(fileName, valueIndex):
         # it also ignores the last line of the file, because the last line is blank
         values = [float(line.split()[valueIndex]) for line in lines[:-1]]
 
+
+# We use this function to plot graphs that show how NTP's actual error in time
+# synchronization compares to the uncertainty range that it provides. The 
+# actual error is the difference between the "real" network time and what NTP
+# thinks the time is. Since that difference can be positive or negative, we
+# plot the uncertainty and the uncertainty negated, so that we can see if the
+# error stays within the uncertainty range.
 def plotNTPVsRealOffset():
-    with open(realOffsetFileName, 'r') as offsetFile:
-        with open(ntpOffsetFileName, 'r') as ntpFile:
 
-            realOffsetFileContents = offsetFile.read()
-            ntpFileContents = ntpFile.read()
+    ntpOffsetValues = getValuesFromFile(ntpOffsetFilePath, 1)
+    realOffsetValues = getValuesFromFile(realOffsetValues, 1)
+    errorValues = getValuesFromFile(maxErrorFilePath, 1)
+    # These values are in microseconds; we want them in seconds.
+    errorValues = [x/1000000.0 for x in errorValues]
 
-            realOffsetLines = str.split(realOffsetFileContents, "\n")
-            ntpOffsetLines = str.split(ntpFileContents, "\n")
+    # stores the difference between the NTP offset and the real offset
+    offsetDiffValues = []
 
-            # We pull out the NTP offset and real offset values from the 2nd node 
-            ntpOffsetValues = [float(x.split()[1]) for x in realOffsetLines[:-1]]
-            realOffsetValues = [float(x.split()[1]) for x in ntpOffsetLines[:-1]]
+    for i in range(len(ntpOffsetValues)):
+        offsetDiffValues += [ntpOffsetValues[i] - realOffsetValues[i]]
 
-            # stores the difference between the NTP offset and the real offset
-            offsetDiffValues = []
+    # We make 1 second time values from 0 to 20001
+    timeValues = np.arange(0., 20001., 1)
 
-            for i in range(len(ntpOffsetValues)):
-                offsetDiffValues += [ntpOffsetValues[i] - realOffsetValues[i]]
-
-            # We make 1 second time values from 0 to 20001
-            timeValues = np.arange(0., 20001., 1)
-
-            plt.plot(timeValues, offsetDiffValues, 'g')
-
-            errorFile = open(maxErrorFileName, 'r')
-            errorContents = errorFile.read()
-            errorFile.close()
-
-            errorLines = str.split(errorContents, "\n")
-            errorValues = [float(x.split()[1])/1000000.0 for x in errorLines[:-1]]
-
-            plt.plot(timeValues, errorValues, 'b')
-            # We also want to plot the negative values
-            negErrorValues = [(-1.0 * x) for x in errorValues]
-            plt.plot(timeValues, negErrorValues, 'r')
+    plt.plot(timeValues, offsetDiffValues, 'g')
+    plt.plot(timeValues, errorValues, 'b')
+    # We also want to plot the negative values
+    negErrorValues = [(-1.0 * x) for x in errorValues]
+    plt.plot(timeValues, negErrorValues, 'r')
 
 
-            plt.axis([0, 20000, -0.01, 0.01])
-            # plot a line at 0
-            plt.plot(timeValues, [0]*len(timeValues), '-')
+    plt.axis([0, 20000, -0.01, 0.01])
+    # plot a line at 0
+    plt.plot(timeValues, [0]*len(timeValues), '-')
 
-            plt.xlabel("Time (s)")
-            plt.ylabel("Offset, Uncertainty (s)")
-            plt.title("(NTP Offset - True Offset) vs Time")
-            pylab.savefig("images/offsetvstime.png")
-            plt.clf()
+    plt.xlabel("Time (s)")
+    plt.ylabel("Offset, Uncertainty (s)")
+    plt.title("(NTP Offset - True Offset) vs Time")
+    pylab.savefig("images/offsetvstime.png")
+    plt.clf()
 
+
+# This function plots uncertainty values and network latency values together
+# over time to compare how those values are related.
 def plotLatencyVsError():
-    ntpFile = open(ntpOffsetFileName, 'r')
-    ntpContents = ntpFile.read()
-    ntpFile.close()
 
-    ntpLines = str.split(ntpContents, "\n")
-    # we need to convert the values to seconds rather than microseconds
-    ntpValues = [float(x.split()[1]) for x in ntpLines[:-1]]
+    ntpValues = getValuesFromFile(ntpOffsetFilePath, 1)
 
     # We make 1 second time values from 0 to 20001
     timeValues = np.arange(0., 20001., 1)
 
     plt.plot(timeValues, ntpValues, 'r')
 
-    packetFile = open(packetDelayFileName, 'r')
-    packetContents = packetFile.read()
-    packetFile.close()
+    # The lines that contain the packet being sent/received information
+    # We instantiate these in the next block.
+    sendLines = []
+    recLines = []
 
-    allPacketLines = [x.split() for x in packetContents.split("\n")]
-    # We look to see if the second node is in the send or receive column
-    # If so, we put that line in the corresponding list of lines from the file.
-    sendLines = [x for x in allPacketLines[:-1] if (int(x[1]) == 2)]
-    recLines = [x for x in allPacketLines[:-1] if (int(x[2]) == 2)]
+    with open(packetDelayFilePath, 'r') as packetFile:
+        packetContents = packetFile.read()
 
+        allPacketLines = [x.split() for x in packetContents.split("\n")]
+        # We look to see if the second node is in the send or receive column
+        # If so, we put that line in the corresponding list of lines from the file.
+        sendLines = [x for x in allPacketLines[:-1] if (int(x[1]) == 2)]
+        recLines = [x for x in allPacketLines[:-1] if (int(x[2]) == 2)]
+
+    # We want to look at the round trip time the packet took to go from the
+    # node to the server and back. We exclude time that the packet spent in
+    # the server.
     roundTripValues = []
     for i in range(len(recLines) - 1):
         sendTime = float(sendLines[i][0])
@@ -100,8 +101,10 @@ def plotLatencyVsError():
 
         sendDelay = float(sendLines[i][3])
         recDelay = float(recLines[i][3])
-        # We want to create (nextSendTime - sendTime) values of the round
-        # trip time so that our plot will draw a line at those times.
+        # To ensure the plot looks good, we need to add N values of the 
+        # round trip time to roundTripValues, where:
+        #       N = (nextSendTime - sendTime) 
+
         roundTripValues += [sendDelay + recDelay]*(int(nextSendTime - sendTime))
 
     plt.plot(timeValues[:len(roundTripValues)], roundTripValues, 'g.')
@@ -112,61 +115,74 @@ def plotLatencyVsError():
     plt.title("Uncertainty and Latency vs. Time")
     pylab.savefig("images/latency.png")
 
+
+# This function plots four values:
+# 1) NTP's estimate for the clock's offset from "real" network time
+# 2) The clock's actual offset from "real" network time
+# 3) NTP's uncertainty in what the real time is
+# 4) The Safety Buffer: the difference between NTP's uncertainty and the
+#    difference between NTP's estimate for the offset and the actual offset.
+#    More mathematically, using the numbering here to represent the values:
+#             Safety Buffer = [3] - |[2] - [1]|
+# So long as the safety buffer is positive, then NTP is functioning as it is
+# supposed to. If there is ever a moment when NTP fails to maintain the error
+# within its uncertainty (i.e. the safety buffer is negative), a warning is 
+# printed, with the value index and the value.
+#
+# If there is more than one node in the system, then the values are plotted
+# sequentially, i.e. the first node's values are plotted first, then the second
+# node, then the third, etc.
+#
+# After the plot is made, five values are printed: the safety buffers maximum
+# value, its minimum value, its average, and its average +/- its standard
+# deviation. 
 def extractOverviewOffsetData():
-    offsetFile = open(realOffsetFileName, 'r')
-    ntpFile = open(ntpOffsetFileName, 'r')
-    errorFile = open(maxErrorFileName, 'r')
-
-    realOffsetFileContents = offsetFile.read()
-    ntpFileContents = ntpFile.read()
-    errorContents = errorFile.read()
-
-    offsetFile.close()
-    ntpFile.close()
-    errorFile.close()
-
-    realOffsetLines = str.split(realOffsetFileContents, "\n")
-    ntpOffsetLines = str.split(ntpFileContents, "\n")
-    errorLines = str.split(errorContents, "\n")
-
-    # We pull out the NTP offset and real offset values from the 2nd node 
-    # We skip the last line in these values because it is an empty value
     ntpOffsetValues = []
     realOffsetValues = []
     errorValues = []
-    offsetBufferValues = []
-    for i in range(1,9):
-        ntpOffsetValues += [float(x.split()[i]) for x in ntpOffsetLines[1500:-1]]
-        realOffsetValues += [float(x.split()[i]) for x in realOffsetLines[1500:-1]]
-        errorValues += [float(x.split()[i])/1000000.0 for x in errorLines[1500:-1]]
+    safetyBufferValues = []
+
+    numNodes = len(ntpOffsetLines[0].split())
+    # We ignore the first 1500 values because NTP needs to take that time to
+    # synchronize the nodes. Those values aren't valid.
+    for i in range(1, numNodes):
+        ntpOffsetValues += getValuesFromFile(ntpOffsetFilePath, i)[1500:]
+        realOffsetValues += getValuesFromFile(realOffsetFilePath, i)[1500:]
+        errorValues += getValuesFromFile(maxErrorFilePath, i)[1500:]
+
+    # We convert the errorValues from microseconds to seconds.
+    errorValues = [x/1000000.0 for x in errorValues]
+
     for i in range(len(ntpOffsetValues)):
-        if (errorValues[i] - abs(ntpOffsetValues[i] - realOffsetValues[i])) <= 0:
-            print "BELOW 0!!!!"
-            print i
-            print errorValues[i] - abs(ntpOffsetValues[i] - realOffsetValues[i])
-        offsetBufferValues += [errorValues[i] - abs(ntpOffsetValues[i] - realOffsetValues[i])]
+        safetyBufferValue = errorValues[i] - abs(ntpOffsetValues[i] - realOffsetValues[i])
+        if safetyBufferValue < 0:
+            print "Warning: Negative Safety Buffer Found"
+            print "\tValue Index: ", i
+            print "\tValue:", safetyBufferValue
+        safetyBufferValues += [safetyBufferValue]
 
-  #  plt.plot(ntpOffsetValues, 'r')
-  #  plt.plot(realOffsetValues, 'g')
-  #  plt.plot(errorValues, 'b')
- #   plt.plot(offsetBufferValues, 'y')
+    plt.plot(ntpOffsetValues, 'r')
+    plt.plot(realOffsetValues, 'g')
+    plt.plot(errorValues, 'b')
+    plt.plot(safetyBufferValues, 'y')
 
-    average = sum(errorValues) / float(len(errorValues))
-    stdDev = np.std(errorValues)
+    average = sum(safetyBufferValues) / float(len(safetyBufferValues))
+    stdDev = np.std(safetyBufferValues)
 
-    print "Max:", max(errorValues)
+    print "Safety Buffer Stats"
+    print "Max:", max(safetyBufferValues)
     print "+Standard Deviation:", average + stdDev
     print "Average:", average
     print "-Standard Deviation:", average - stdDev
-    print "Min:", min(errorValues)
+    print "Min:", min(safetyBufferValues)
 
-#    plt.show()
+    plt.show()
 
-
+# uncomment the desired function to run it.
 def main():
     # plotNTPVsRealOffset()
     # plotLatencyVsError()
-    extractOverviewOffsetData()
+    # extractOverviewOffsetData()
 
 
 if __name__ == "__main__":
